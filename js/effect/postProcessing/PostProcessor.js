0e46bc3e89e8168b3bf25fd764f40881d8251dc0
Iriska <yet.iriska@gmail.com>
2014-09-10 00:20:03 +0800

    some work on post processor

--- js/effect/postProcessing/PostProcessor.js
+++ js/effect/postProcessing/PostProcessor.js
@@ -10,9 +10,11 @@ var PostProcessor = (function (){
 		//public
 	
 		this.renderer = renderer;
-		this.camera;
-		this.targetCanvas;
-		this.scene;
+		this.camera = new THREE.OrthographicCamera( -1, 1, 1, -1, 0, 1 );
+		this.scene = new THREE.Scene();
+		this.artbord = new THREE.Mesh( new THREE.PlaneGeometry( 2, 2 ), null );
+		this.renderToScreen = true;
+		this.currentTime = (new Date()).valueOf();
 
 		if ( renderTarget === undefined ){
 			var width = window.innerWidth || 1;
@@ -44,23 +46,19 @@ var PostProcessor = (function (){
 
 	};
 
+	
+})();
 
 
-	this.swapBuffers = function () {
 
-		var tmpBuffer = this.inputBuffer;
-		this.inputBuffer = this.outputBuffer;
-		this.outputBuffer = tmpBuffer;
+PostProcessor.prototype = {
+	constructor: PostProcessor,
 
-	};
+	initProessors: function () {
 
+	},
 
-	this.initProcessors = function() {
-
-	};
-
-
-	this.reset = function ( renderTarget ) {
+	reset: function ( renderTarget ) {
 
 		if ( renderTarget === undefined ){
 			renderTarget = this.renderTarget1.clone( );
@@ -75,20 +73,43 @@ var PostProcessor = (function (){
 		this.inputBuffer = this.renderTarget1;
 		this.outputBuffer = this.renderTarget2;
 
-	};
+	},
 
+	render: function ( scene, camera, forceClear, inputBuffer, outputBuffer ) {
 
-	this.render = function ( scene, camera, renderToScreen, forceClear ) {
+		var newTime = (new Date()).valueOf();
+		var delta_time = newTime - this.currentTime;
+		this.currentTime = newTime;
 
-		this.renderer.render( scene, camera, renderTarget, forceClear );
+		this.renderer.render( scene, camera, this.inputBuffer, forceClear );
 
-	};
+		for (var i = 0; i < this.post_processors.length; ++i){
+			this.post_processors[i].uniforms[ "tDiffuse" ].value = this.inputBuffer;
 
-	
-})();
+			if( this.post_processors[i].uniforms[ "delta_time" ] !== undefined )
+				this.post_processors[i].uniforms[ "delta_time" ].value = delta_time;
 
+			this.artbord.material = this.post_processors[i];
+			this.renderer.render( this.scene, this.camera, this.outputBuffer, forceClear );
 
+			if( i != this.post_processors.length - 1 )
+				this.swapBuffers();
+			else
+				if( this.renderToScreen )
+					this.renderer.render( this.scene, this.camera );
+		}	
 
-PostProcessor.prototype = {
-	constructor: PostProcessor,
+	},
+
+	addProcessor: function ( shader ) {
+		this.post_processors.push( shader );
+	},
+
+	swapBuffers: function () {
+
+		var tmpBuffer = this.inputBuffer;
+		this.inputBuffer = this.outputBuffer;
+		this.outputBuffer = tmpBuffer;
+
+	}
 }
