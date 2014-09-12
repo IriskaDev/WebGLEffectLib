9142daa17e0c3dc413786cd451652dcf2ad541f7
Iriska <yet.iriska@gmail.com>
2014-09-12 23:55:16 +0800

    change some file's dir

---
+++ js/shader/passShaders/blurPass.js
@@ -0,0 +1,27 @@
+THREE.GLEffectLib.Shaders.blurPass = {
+	shader_name: "blurPass",
+	uniform_list: {
+		tDiffuse: { type: "t", value: null },
+		noiseMap: { type: "t", value: null }
+	},
+	vx_shader: "\n\
+		varying vec2 vUv;\n\
+		void main(void){\n\
+			vUv = uv; \n\
+			vec4 mvPosition = modelViewMatrix * vec4( position, 1.0 );\n\
+			gl_Position = projectionMatrix * mvPosition;\n\
+		}\
+	",
+	fg_shader: "\n\
+		#ifdef GL_ES\n\
+		precision highp float;\n\
+		#endif\n\
+		uniform sampler2D tDiffuse;\n\
+		uniform sampler2D noiseMap;\n\
+		\n\
+		varying vec2 vUv;\n\
+		void main(void) { \n\
+			
+		}\
+	"
+};
