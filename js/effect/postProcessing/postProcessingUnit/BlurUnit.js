ab960740da5fd47ab2b737ef2968fbc825330b8e
Iriska <yet.iriska@gmail.com>
2014-09-26 15:27:33 -0700

    a simple blur pass and it's demo

--- js/effect/postProcessing/postProcessingUnit/BlurUnit.js
+++ js/effect/postProcessing/postProcessingUnit/BlurUnit.js
@@ -1,7 +1,18 @@
 THREE.GLEffectLib.PostProcessUnit.BlurUnit = (function (){
 
-	return function (){
-		this.material = null
+	return function (blurLev, resolutionX, resolutionY){
+		this.material = new THREE.ShaderMaterial({
+			uniforms: THREE.GLEffectLib.Shaders.BlurPass.uniforms,
+			vertexShader: THREE.GLEffectLib.Shaders.BlurPass.vx_shader,
+			fragmentShader: THREE.GLEffectLib.Shaders.BlurPass.fg_shader
+		});
+
+		this.blurLev = blurLev;
+
+		this.material.uniforms["uResolutionXRec"].value = (resolutionX === undefined ? 1.0 / window.width: 1.0 / resolutionX);
+		this.material.uniforms["uResolutionYRec"].value = (resolutionY === undefined ? 1.0 / window.height: 1.0 / resolutionY);
+		this.material.uniforms["uCountX"].value = this.blurLev * 2 + 1;
+		this.material.uniforms["uCountY"].value = this.blurLev * 2 + 1;
 	}
 
 })();
@@ -11,6 +22,7 @@ THREE.GLEffectLib.PostProcessUnit.BlurUnit.prototype = {
 
 	configMaterial: function ( delta_time ) {
 
+		//return this.blurArr;
 		return this.material;
 
 	}
--- js/shader/passShaders/blurPass.js
+++ js/shader/passShaders/blurPass.js
@@ -1,8 +1,11 @@
 THREE.GLEffectLib.Shaders.BlurPass = {
 	shader_name: "blurPass",
-	uniform_list: {
+	uniforms: {
 		tDiffuse: { type: "t", value: null },
-		noiseMap: { type: "t", value: null }
+		uResolutionXRec: { type: "f", value: 0 },   // 1.0 / ResolutionX
+		uResolutionYRec: { type: "f", value: 0 },	// 1.0 / ResolutionY
+		uCountX: { type: "i", value: 7 },
+		uCountY: { type: "i", value: 7 }
 	},
 	vx_shader: "\n\
 		varying vec2 vUv;\n\
@@ -17,11 +20,33 @@ THREE.GLEffectLib.Shaders.BlurPass = {
 		precision highp float;\n\
 		#endif\n\
 		uniform sampler2D tDiffuse;\n\
-		uniform sampler2D noiseMap;\n\
+		uniform float uResolutionXRec;\n\
+		uniform float uResolutionYRec;\n\
+		uniform int uCountX;\n\
+		uniform int uCountY;\n\
 		\n\
 		varying vec2 vUv;\n\
+		\n\
+		\n\
 		void main(void) { \n\
-			
+			vec3 tmpColor = vec3( 0.0, 0.0, 0.0 );\n\
+				//for ( float i = vUv.x - uRadius; i < vUv.x + uRadius; i += uResolutionXRec ) { \n\
+				//	for ( float j = vUv.y - uRadius; j < vUv.y + uRadius; j += uResolutionYRec ) { \n\
+				//		tmpColor += texture2D( tDiffuse, vUv ).rgb;\n\
+				//	}\n\
+				//}\n\
+				for ( int i = 0; i < 64; ++i ) {\n\
+					if( i >= uCountX ) {\n\
+						break;\n\
+					}\n\
+					for(int j = 0; j < 64; ++j ) {\n\
+						if( j >= uCountY ) {\n\
+							break;\n\
+						}\n\
+						tmpColor += texture2D( tDiffuse, vec2(vUv.x - (float(i) - float(uCountX) / 2.0) * uResolutionXRec, vUv.y - (float(j) - float(uCountY) / 2.0) * uResolutionYRec) ).rgb; \n\
+					}\n\
+				}\n\
+			gl_FragColor = vec4( tmpColor / float(uCountX * uCountY), 1.0 );\n\
 		}\
 	"
 };
